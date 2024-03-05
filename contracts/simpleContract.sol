9aff9b229b4bd9aa694adba3078fdd7d70cd3659
kateberryd <kateberryd@gmail.com>
2024-03-05 09:57:59 +0100

    refactor: deleted some files from contract

---
+++ contracts/simpleContract.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+contract SimpleContract {
+
+    string fullName;
+    uint256 sumOfTwoNumbers;
+    uint256 dividedNum;
+
+    /**
+    * @dev  modifies name / changes state
+    */
+
+    function storeName (string memory name) public {
+        fullName = name;
+    }
+
+    /**
+    * @dev returns fullName form the state
+    */
+
+    function getFullName() public view returns (string memory) {
+        return  fullName;
+    }
+
+   /**
+    * @dev recieves two positive intergers sum them up and save the result 
+    */
+
+    function sumNumbers (uint256 a, uint256 b) public {
+        sumOfTwoNumbers = a + b;
+    }
+
+   /**
+    * @dev retrieves the sum of two vales
+    */
+
+
+    function getSumOfNumbers () public  view  returns (uint256) {
+        return  sumOfTwoNumbers;
+    }
+
+
+   /**
+    * @dev accept two numbers then divide and store the result
+    */
+
+    function divideNumbers (uint256 a, uint256 b) public {
+        dividedNum = a / b;
+    }
+
+
+   /**
+    * @dev retrieves the divided result from the state
+    */
+
+
+    function getDividedNum() public view returns  (uint256) {
+        return  dividedNum;
+        
+    }
+    
+
+   
+}
